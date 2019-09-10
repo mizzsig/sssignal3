966d5ceceada86ae48182af0e1816bb7e23e5826
mizzsig <ihrwon@yahoo.co.jp>
2019-09-10 22:28:48 +0900

    コンポーネントの出し分け

--- app/front/pages/index.vue
+++ app/front/pages/index.vue
@@ -1,19 +1,24 @@
 <template>
   <section class="container">
-    <div class="button" @click="prevScene">←</div>
-    {{ scene }}
-    <div class="button" @click="nextScene">→</div>
-    {{ render }}
+    <scene1 v-if="scene === 1"></scene1>
+    <scene2 v-if="scene === 2"></scene2>
   </section>
 </template>
 
 <script>
+import Vue from 'vue';
+import Scene1 from '../components/index/scene1.vue';
+import Scene2 from '../components/index/scene2.vue';
+
 export default {
+    components: {
+        Scene1,
+        Scene2,
+    },
   data() {
     return {
       scene: 1,
-      components: [],
-      render: "<div>test</div>"
+      components: 2
     }
   },
   head() {
@@ -21,35 +26,30 @@ export default {
       title: "sssignal3 Teaser Website"
     };
   },
+  beforeMount() {
+      this.scene = Math.floor(Math.random() * this.components) + 1;
+  },
   methods: {
-    prevScene() {
-      this.scene--;
-      console.log(this.scene);
-    },
-    nextScene() {
-      this.scene++;
-      console.log(this.scene);
-    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.container {
-  display: inline-flex;
-  max-height: 100%;
-}
-img {
-  max-width: 100%;
-  max-height: 100%;
-}
+// .container {
+//   display: inline-flex;
+//   max-height: 100%;
+// }
+// img {
+//   max-width: 100%;
+//   max-height: 100%;
+// }
 
-.button {
-    width: 100px;
-    height: 20px;
-    cursor: pointer;
-    border: 1px solid #777777;
-    border-radius: 4px;
-    background-color: #555555;
-}
+// .button {
+//     width: 100px;
+//     height: 20px;
+//     cursor: pointer;
+//     border: 1px solid #777777;
+//     border-radius: 4px;
+//     background-color: #555555;
+// }
 </style>
