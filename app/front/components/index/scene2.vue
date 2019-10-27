b0130a84cdb1667f28af4ca37f43601c1e21a0f6
mizzsig <ihrwon@yahoo.co.jp>
2019-10-27 23:54:02 +0900

    ピアノのブラウザ依存のバグいくつか修正

--- app/front/components/index/scene2.vue
+++ app/front/components/index/scene2.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>水飴信号3 ~Starch Syrup Signal~</h1>
         <div class="keyboard-container" style="margin-bottom: -66px;">
-            <div :id="'black' + index" v-for="(value, index) in this.keyboardsBlack" v-bind:key="index" @mousedown="playPiano(index, 'black')" @mouseup="pianoMouseUp(index, 'black')" @mouseleave="pianoMouseUp(index, 'black')" :style="isBlackShow(index)" class="keyboard black">
+            <div :id="'black' + index" v-for="(value, index) in this.keyboardsBlack" v-bind:key="index" @mousedown="playPiano(index, 'black')" @mouseup="pianoMouseUp(index, 'black')" @mouseleave="pianoMouseUp(index, 'black')" :style="isBlackMargin(index)" class="keyboard black">
             </div>
         </div>
         <div class="keyboard-container">
@@ -13,16 +13,17 @@
 </template>
 
 <script>
-const whiteKeyCodes = [90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 167, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 64];
-const blackKeyCodes = [83, 68, 70, 71, 72, 74, 75, 76, 59, 58, 221, 50, 51, 52, 53, 54, 55, 56, 57, 48, 173, 160];
+const whiteKeys = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '@', '['];
+const blackKeys = ['s', 'd', 'g', 'h', 'j', 'l', ';', '2', '3', '4', '6', '7', '9', '0', '-'];
 
 export default {
     data() {
         return {
             windowWidth: 0,
+            // 表示しているキーボードを格納する配列、押されているときはtrue、押されてないときはfales
             keyboardsBlack: [],
             keyboardsWhite: [],
-            hiddenBlackKey: [2, 6, 9, 13, 16, 20],
+            hiddenBlackKey: [2, 5, 7, 10, 12, 15, 17, 20],
         }
     },
     mounted() {
@@ -37,7 +38,8 @@ export default {
         // 画面サイズに応じた鍵盤を表示する
         onResize() {
             const countWhite = parseInt(window.innerWidth / 26) > 22 ? 22 : parseInt(window.innerWidth / 26);
-            const countBlack = countWhite - 1;
+            const blackKeyBoards = [0, 0, 1, 2, 2, 3, 4, 5, 5, 6, 7, 7, 8, 9, 10, 10, 11, 12, 12, 13, 14, 15, 15];
+            const countBlack = blackKeyBoards[countWhite];
             if (countWhite > this.keyboardsWhite.length) {
                 const tmp = Array(countWhite - this.keyboardsWhite.length).fill(false);
                 this.keyboardsWhite = this.keyboardsWhite.concat(tmp);
@@ -51,10 +53,12 @@ export default {
                 this.keyboardsBlack = this.keyboardsBlack.splice(0, countBlack);
             }
         },
-        // 黒鍵盤を見せるかどうかの判定
-        isBlackShow(index) {
-            if (this.hiddenBlackKey.indexOf(index) >= 0) {
-                return { opacity: 0 };
+        // 黒鍵盤の左側にマージンを入れるかどうかの判定
+        isBlackMargin(index) {
+            if (index === 0) {
+                return { marginLeft: '-23px' };
+            } else if (this.hiddenBlackKey.indexOf(index) >= 0) {
+                return { marginLeft: '29px' };
             }
             return {};
         },
@@ -75,19 +79,25 @@ export default {
         },
         // キーボードからピアノ鍵盤が押されたかを判定する
         pianoKeyDown(event) {
-            if (whiteKeyCodes.indexOf(event.keyCode) >= 0) {
-                this.playPiano(whiteKeyCodes.indexOf(event.keyCode), 'white');
-            } else if (blackKeyCodes.indexOf(event.keyCode) >= 0) {
-                this.playPiano(blackKeyCodes.indexOf(event.keyCode), 'black');
+            if (whiteKeys.indexOf(event.key) >= 0 && !this.keyboardsWhite[event.key]) {
+                this.keyboardsWhite[event.key] = true;
+                event.preventDefault();
+                this.playPiano(whiteKeys.indexOf(event.key), 'white');
+            } else if (blackKeys.indexOf(event.key) >= 0 && !this.keyboardsBlack[event.key]) {
+                this.keyboardsBlack[event.key] = true;
+                event.preventDefault();
+                this.playPiano(blackKeys.indexOf(event.key), 'black');
             }
         },
         // キーボードからピアノ鍵盤が離されたかを判定する
         pianoKeyUp(event) {
-            if (whiteKeyCodes.indexOf(event.keyCode) >= 0) {
-                const key = document.getElementById('white' + whiteKeyCodes.indexOf(event.keyCode));
+            if (whiteKeys.indexOf(event.key) >= 0) {
+                const key = document.getElementById('white' + whiteKeys.indexOf(event.key));
+                this.keyboardsWhite[event.key] = false;
                 key.classList.remove('keydown');
-            } else if (blackKeyCodes.indexOf(event.keyCode) >= 0) {
-                const key = document.getElementById('black' + blackKeyCodes.indexOf(event.keyCode));
+            } else if (blackKeys.indexOf(event.key) >= 0) {
+                const key = document.getElementById('black' + blackKeys.indexOf(event.key));
+                this.keyboardsBlack[event.key] = false;
                 key.classList.remove('keydown');
             }
         }
