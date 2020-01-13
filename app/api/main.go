07d9a95b476594e68eac2ba786405e7785c4a683
mizzsig <ihrwon@yahoo.co.jp>
2020-01-14 01:09:08 +0900

    formから問い合わせを送れるようにした

--- app/api/main.go
+++ app/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/joho/godotenv"
 	"io/ioutil"
 	"encoding/json"
@@ -11,12 +12,22 @@ import (
 	"fmt"
 )
 
+// 問い合わせフォームから送られるリクエスト
+type InquiryRequest struct {
+	Address string `json:"address"`
+	Body string `json:"body"`
+}
+
+// Slack webhookに渡す用の構造体
 type Slack struct {
 	Text string `json:"text"`
 }
 
 func main() {
 	e := echo.New()
+
+	// CORS用の設定
+	e.Use(middleware.CORS())
 
 	// .envファイル読み込み
 	err := godotenv.Load(".env")
@@ -29,15 +40,17 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, world!!!")
 	})
-	e.GET("/inquiry", func(c echo.Context) error {
+	e.POST("/inquiry", func(c echo.Context) error {
 		// パラメータを受け取る
-		address := c.FormValue("address")
-		text := c.FormValue("text")
+		inquiryRequest := new(InquiryRequest)
+		if err := c.Bind(inquiryRequest); err != nil {
+			return c.String(http.StatusOK, "err!")
+		}
 
 		// TODO : 見た目もう少し良くしたい 
 		// https://api.slack.com/tools/block-kit-builder
 		slack := new(Slack)
-		slack.Text = "*【問い合わせフォーム】*\naddress : *" + address + "*\ntext : " + text
+		slack.Text = "*【問い合わせフォーム】*\naddress : *" + inquiryRequest.Address + "*\ntext : " + inquiryRequest.Body
 
 		// 構造体をJSONに変換
 		json, _:= json.Marshal(slack)
@@ -48,13 +61,13 @@ func main() {
 		// Slack webhookにPOSTリクエスト送る
 		response, err := http.PostForm(slackWebhook, url.Values{"payload": {string(json)}})
 		// レスポンス読み込み
-		body, err := ioutil.ReadAll(response.Body)
+		responseBody, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			return c.String(http.StatusOK, "err!")
 		}
 
 		// webhookの結果をJSONで返す
-		return c.JSON(http.StatusOK, map[string]string{"result": string(body)})
+		return c.JSON(http.StatusOK, map[string]string{"result": string(responseBody)})
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
